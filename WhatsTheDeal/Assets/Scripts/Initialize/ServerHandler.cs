01ace1f4bc150f7605effa73a687c99bc86ae39b
hazlett <hazlett.206@gmail.com>
2015-01-07 15:11:36 -0500

    Login

---
+++ WhatsTheDeal/Assets/Scripts/Initialize/ServerHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerHandler : MonoBehaviour {
+    private static ServerHandler instance;
+    public static ServerHandler Instance { get { return instance; } }
+
+    private User user;
+    internal User CurrentUser { get { return user; } }
+    private bool authenticating, authenticated;
+    internal bool Authenticating { get { return authenticating; } }
+    internal bool Authenticated { get { return authenticated; } }
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    internal void SetUser(User user)
+    {
+        this.user = user;
+    }
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    internal void StartAuthenticate()
+    {
+        authenticated = false;
+        authenticating = true;
+        StartCoroutine("Authenticate");
+    }
+    private IEnumerator Authenticate()
+    {
+        yield return null;
+        authenticating = false;
+        authenticated = true;
+    }
+}
---
+++ WhatsTheDeal/Assets/Scripts/Initialize/Splash.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+    private float timer;
+    private enum LoadState
+    {
+        LOADING,
+        USERFOUND,
+        NOUSER
+    }
+    private LoadState loadState;
+    void Start()
+    {
+        timer = 0;
+        loadState = LoadState.LOADING;
+        StartCoroutine("CheckUser");
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if ((timer > 3) && (loadState != LoadState.LOADING))
+        {
+            switch (loadState)
+            {
+                case LoadState.NOUSER:
+                    Application.LoadLevel("Login");
+                    break;
+                case LoadState.USERFOUND:
+                    Application.LoadLevel("Main");
+                    break;
+                default:
+                    Application.LoadLevel("Login");
+                    break;
+            }
+        }
+    }
+    private IEnumerator CheckUser()
+    {
+        yield return null;
+        User user = new User();
+        user = user.Load();
+        if (user != null)
+        {
+            if (user.AutoLogin == "true")
+            {
+                ServerHandler.Instance.SetUser(user);
+                loadState = LoadState.USERFOUND;
+            }
+            else
+            {
+                loadState = LoadState.NOUSER;
+            }
+        }
+        else
+        {
+            loadState = LoadState.NOUSER;
+        }
+
+    }
+    void OnGUI()
+    {
+
+        GUILayout.Label("<b>WHAT'S THE DEAL</b>");
+        GUILayout.Label("Produced by Ben and David Studios");
+
+        GUILayout.Label("\n\n\n\n\n...LOADING...");
+
+    }
+}
